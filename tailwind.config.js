a85674e0265c168ec00afc01b367b4d41d861f6e
Gabriel <gam98lhda@gmail.com>
2022-01-19 15:48:41 -0300

    ready

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    colors: {
+      //Primary colors
+      cyan: "hsl(179, 62%, 43%)",
+      cyanOpacity: "hsla(179, 62%, 43%,0.7)",
+      brightYellow: "hsl(71, 73%, 54%)",
+      //Neutral colors
+      lightGray: "hsl(204, 43%, 93%)",
+      lightGrayOpacity: "hsla(204, 43%, 93%,0.7)",
+      grayishBlue: "hsl(218, 22%, 67%)",
+      white: "#fff",
+      frontendMentor: "hsl(228, 45%, 44%)",
+    },
+    fontFamily: {
+      karla: "Karla",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
